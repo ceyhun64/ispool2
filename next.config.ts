ab0533b4e42312ad6bb7d957328ce8d2798b657a
Ceyhun Turkmen <ctrkmn64@gmail.com>
2026-02-23 14:43:20 +0300

    91. commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["fluent-ffmpeg", "@ffmpeg-installer/ffmpeg"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        pathname: "/**", // Tüm alt yollara izin verir
+        pathname: "/**",
       },
     ],
   },
